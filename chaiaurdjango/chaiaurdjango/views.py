530f3df3baeb6949dd2adc48a4facc8c676db4c9
omkaaaaaar <omkarpatkarr@gmail.com>
2025-09-10 22:46:10 +0530

    sq

--- chaiaurdjango/chaiaurdjango/views.py
+++ chaiaurdjango/chaiaurdjango/views.py
@@ -5,7 +5,8 @@ def home(request):
     #return HttpResponse("Hello, World! Welcome to Chaiaur Django Home Page!")
     return render(request, 'website/index.html')
 def about(request):
-    return HttpResponse("Hello, World! Welcome to Chaiaur Django about Page!")
-
+    #return HttpResponse("Hello, World! Welcome to Chaiaur Django about Page!")
+    return render(request, 'about/about.html')
 def contact(request):
-    return HttpResponse("Hello, World! Welcome to Chaiaur Django contact Page!") 
+   # return HttpResponse("Hello, World! Welcome to Chaiaur Django contact Page!") 
+   return render(request, 'contact/contact.html')
