8101ba9f15e527afffd146520527e177d3080d9e
omkaaaaaar <omkarpatkarr@gmail.com>
2025-09-10 20:23:19 +0530

    Update views.py

--- chaiaurdjango/chaiaurdjango/views.py
+++ chaiaurdjango/chaiaurdjango/views.py
@@ -1,8 +1,9 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def home(request):
-    return HttpResponse("Hello, World! Welcome to Chaiaur Django Home Page!")
-
+    #return HttpResponse("Hello, World! Welcome to Chaiaur Django Home Page!")
+    return render(request, 'index.html')
 def about(request):
     return HttpResponse("Hello, World! Welcome to Chaiaur Django about Page!")
 
