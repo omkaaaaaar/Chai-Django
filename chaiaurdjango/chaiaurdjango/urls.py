38b358a5ba4dce210be07dbb01ba52458add2192
omkaaaaaar <omkarpatkarr@gmail.com>
2025-09-10 20:07:13 +0530

    m

--- chaiaurdjango/chaiaurdjango/urls.py
+++ chaiaurdjango/chaiaurdjango/urls.py
@@ -17,7 +17,8 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path
-from chaiaurdjango import views
+from . import views
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('', views.home, name='home'),
