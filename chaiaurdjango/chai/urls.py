436f26575b99335f586e509b5b40cf5f84d7f2f1
omkaaaaaar <omkarpatkarr@gmail.com>
2025-09-24 21:27:53 +0530

    Update urls.py

--- chaiaurdjango/chai/urls.py
+++ chaiaurdjango/chai/urls.py
@@ -3,8 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path('', views.all_chai, name='all_chai'),
-   
-    
+    path('', views.all_chai, name='all_chai'),    
 ]
