a2c6b102d9385574252da7458683a11a2e2e537f
omkaaaaaar <omkarpatkarr@gmail.com>
2025-09-24 21:40:20 +0530

    e

--- chaiaurdjango/chai/urls.py
+++ chaiaurdjango/chai/urls.py
@@ -7,5 +7,5 @@ from . import views
 
 urlpatterns = [
     path('', views.all_chai, name='all_chai'),    
-    path('order/', views.order_chai, name='order_chai'),
+    #path('order/', views.order_chai, name='order_chai'),
 ]
