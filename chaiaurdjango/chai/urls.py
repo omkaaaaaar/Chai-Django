31962bd80ac62d0ec4795805ae630caf7612a250
omkaaaaaar <omkarpatkarr@gmail.com>
2025-09-24 21:24:10 +0530

    commit

--- chaiaurdjango/chai/urls.py
+++ chaiaurdjango/chai/urls.py
@@ -4,8 +4,7 @@ from . import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('', views.home, name='home'),
-    path('about/', views.about, name='about'),
-    path('contact/', views.contact, name='contact'),
+    path('', views.all_chai, name='all_chai'),
+   
     
 ]
