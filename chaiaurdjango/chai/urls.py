9031da120442a6c9ba58f87cb20d296a57443a58
omkaaaaaar <omkarpatkarr@gmail.com>
2025-09-24 21:30:06 +0530

    Update urls.py

--- chaiaurdjango/chai/urls.py
+++ chaiaurdjango/chai/urls.py
@@ -2,6 +2,10 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 
+#localhost:8000/chai
+#localhost:8000/chai/order
+
 urlpatterns = [
     path('', views.all_chai, name='all_chai'),    
+    path('order/', views.order_chai, name='order_chai'),
 ]
