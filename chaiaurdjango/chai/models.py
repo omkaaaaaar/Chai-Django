282c18fdc401e1e1a219a42009ef4612b8c82208
omkaaaaaar <omkarpatkarr@gmail.com>
2025-09-27 16:45:47 +0530

    Update models.py

--- chaiaurdjango/chai/models.py
+++ chaiaurdjango/chai/models.py
@@ -2,8 +2,8 @@ from django.db import models
 from django.utils import timezone
 
 # Create your models here.
+
 class ChaiVariety(models.Model):
     name = models.CharField(max_length=100)
     image = models.ImageField(upload_to='chai')
-    date_added = models,models.DateTimeField(default=timezone.now)
-    
+    date_added = models.DateTimeField(default=timezone.now)
