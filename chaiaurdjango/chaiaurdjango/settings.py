29822852fe07f90b00b6163ee3fb186ea8552b8e
omkaaaaaar <omkarpatkarr@gmail.com>
2025-12-01 00:22:28 +0530

    s

--- chaiaurdjango/chaiaurdjango/settings.py
+++ chaiaurdjango/chaiaurdjango/settings.py
@@ -85,12 +85,19 @@ WSGI_APPLICATION = "chaiaurdjango.wsgi.application"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    # "default": {
+    #     "ENGINE": "django.db.backends.sqlite3",
+    #     "NAME": BASE_DIR / "db.sqlite3",
+    # }
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'chaidb',
+        'USER': 'postgres',
+        'PASSWORD': 'Omkar@194',
+        'HOST': 'localhost',
+        'PORT': '',
 }
-
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
